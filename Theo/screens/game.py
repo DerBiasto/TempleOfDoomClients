3c00f9de45db3c2924544e135a3d5ae028a67d93
Theodor Hein <theo@heintech.de>
2026-07-14 01:45:32 +0200

    display cards on role screen

--- Theo/screens/game.py
+++ Theo/screens/game.py
@@ -7,9 +7,7 @@ class GameScreen(Screen):
     def compose(self):
         self.game = lib.GameState()
         with Center():
-            yield Label("Your Cards")
-        with Center():
-            yield Horizontal(Label(card) for card in self.game.cards)
+            yield Label("GameScreen")
 
     def on_mount(self):
         self.set_interval(1, self.game.update())
--- Theo/screens/role.py
+++ Theo/screens/role.py
@@ -1,25 +1,31 @@
 import lib
 from textual.widgets import Label, Button
-from textual.containers import Center
+from textual.containers import Center, Horizontal
 from textual.screen import Screen
 from textual.widgets import Label, Button, Static
 from screens.game import GameScreen
 
 class RoleScreen(Screen):
 
-
     def compose(self):
         self.game = lib.GameState()
         with Center():
-            yield Label("Your Role:")
+            yield Label("Your role is:")
         with Center():
             yield Label(self.game.role + ("🤠" if self.game.role == "Adventurer" else "🛡️"))
         if self.game.role == "Adventurer":
             with Center():
-                yield Static("Your goal is to find all the treasures by communicating with your fellow Adventurers. But beware: there are Guardians trying to lead you into firey traps by using fake announcements, so trust nobody.", id="role_description")
+                yield Static("Your goal is to find all the treasures by communicating with your fellow adventurers. But beware: there are guardians trying to lead you into firey traps by using fake announcements, so trust nobody.", id="role_description")
         else:
             with Center():
-                yield Static("Your goal is to lure the Adventurers into traps by announcing false information", id="role-description")
+                yield Static("Your goal is to lure the adventurers into traps by announcing false information", id="role-description")
+
+        with Center():
+            yield Label("These are your cards:")
+
+        with Center():
+            yield Cards()
+
         with Center():
             yield Button("OK")
 
@@ -29,6 +35,12 @@ class RoleScreen(Screen):
     def on_mount(self):
         self.set_interval(1, self.update_game())
 
-#    def on_button_pressed(self, event: Button.Pressed):
-#        self.app.pop_screen()
-#        self.app.push_screen(GameScreen())
+    def on_button_pressed(self, event: Button.Pressed):
+        self.app.pop_screen()
+        self.app.push_screen(GameScreen())
+
+class Cards(Horizontal):
+    def compose(self):
+        self.game = lib.GameState()
+        for card in self.game.cards:
+            yield Static(card, classes=f"card {card}")
